75b9261d1ea8ed0bd887bc48f04aeedcf1f5854e
minraser1-jpg <minraser1@gmail.com>
2026-04-12 16:36:29 +0300

    Update AppConfig.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
@@ -75,6 +75,10 @@ object AppConfig {
     const val PREF_AUTO_REMOVE_INVALID_AFTER_TEST = "pref_auto_remove_invalid_after_test"
     const val PREF_AUTO_SORT_AFTER_TEST = "pref_auto_sort_after_test"
 
+    // SOCKS5 authentication credentials
+    const val PREF_SOCKS5_USERNAME = "pref_socks5_username"
+    const val PREF_SOCKS5_PASSWORD = "pref_socks5_password"
+
     /** Cache keys. */
     const val CACHE_SUBSCRIPTION_ID = "cache_subscription_id"
 
